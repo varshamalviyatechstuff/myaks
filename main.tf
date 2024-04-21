b10675f311bddd94f56e7d19483ca9a2e4b70632
varshamalviyatechstuff <varshamalviyatechstuff@gmail.com>
2024-04-21 12:29:31 +0200

    add

--- main.tf
+++ main.tf
@@ -1,23 +1,35 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "aks-rg-sa"
+    storage_account_name = "terraformstatefileaccoun"
+    container_name       = "tfstate"
+    key                  = "test.terraform.tfstate"
+  }
+}
 provider "azurerm" {
   features {}
+
 }
+
 resource "azurerm_resource_group" "aks" {
   name     = "myaks-rg"
   location = "North Europe"
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "myaks"
-  location            = "North Europe"
+  name                = "myakscluster"
+  location            = azurerm_resource_group.aks.location
   resource_group_name = azurerm_resource_group.aks.name
-
+  dns_prefix          = "myaksdns"
 
   default_node_pool {
-    name       = "agentpool"
+    name       = "default"
     node_count = 1
     vm_size    = "Standard_DS2_v2"
   }
+
   identity {
     type = "SystemAssigned"
   }
 }
+
