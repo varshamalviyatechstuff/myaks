5039c5ac482157748b7b2c54834916066cd41296
varshamalviyatechstuff <varshamalviyatechstuff@gmail.com>
2024-04-21 11:23:35 +0200

    add files

--- main.tf
+++ main.tf
@@ -1,15 +1,24 @@
 provider "azurerm" {
   features {}
 }
+resource "azurerm_resource_group" "aks" {
+  name     = "myaks-rg"
+  location = "North Europe"
+}
 
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "myaks"
   location            = "North Europe"
   resource_group_name = azurerm_resource_group.aks.name
-  ...
 }
 
-resource "azurerm_resource_group" "aks" {
-  name     = "myaks-rg"
-  location = "North Europe"
+default_node_pool {
+  name       = "agentpool"
+  node_count = 1
+  vm_size    = "Standard_DS2_v2"
+}
+
+
+identity {
+  type = "SystemAssigned"
 }
