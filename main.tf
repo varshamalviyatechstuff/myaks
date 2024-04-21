1ca27794cbbd2bf0adf54db58a73206310bb0e6f
varshamalviyatechstuff <166996406+varshamalviyatechstuff@users.noreply.github.com>
2024-04-21 11:08:00 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,15 @@
+provider "azurerm" {
+  features {}
+}
 
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "myaks"
+  location            = "North Europe"
+  resource_group_name = azurerm_resource_group.aks.name
+  ...
+}
+
+resource "azurerm_resource_group" "aks" {
+  name     = "myaks-rg"
+  location = "North Europe"
+}
